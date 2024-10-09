d8f87fc2f1b72a2c97c67c6b37a1b64cf09b81b7
manuader <adermanu@gmail.com>
2024-10-09 19:32:03 -0300

    Added semantic actions from BisonGramamr.y to BisonActions.h. must implement all actions

--- src/main/c/frontend/syntactic-analysis/BisonActions.h
+++ src/main/c/frontend/syntactic-analysis/BisonActions.h
@@ -7,6 +7,7 @@
 #include "AbstractSyntaxTree.h"
 #include "SyntacticAnalyzer.h"
 #include <stdlib.h>
+#include <stdbool.h>
 
 /** Initialize module's internal state. */
 void initializeBisonActionsModule();
@@ -18,11 +19,57 @@ void shutdownBisonActionsModule();
  * Bison semantic actions.
  */
 
-Constant * IntegerConstantSemanticAction(const int value);
-Expression * ArithmeticExpressionSemanticAction(Expression * leftExpression, Expression * rightExpression, ExpressionType type);
-Expression * FactorExpressionSemanticAction(Factor * factor);
-Factor * ConstantFactorSemanticAction(Constant * constant);
-Factor * ExpressionFactorSemanticAction(Expression * expression);
-Program * ExpressionProgramSemanticAction(CompilerState * compilerState, Expression * expression);
+Program * ProgramSemanticAction(CompilerState * compilerState, Block * block);
+Block * BlockSemanticAction(Instruction * instructions);
+Instruction * SingleInstructionSemanticAction(Instruction * instruction);
+Instruction * MultipleInstructionsSemanticAction(Instruction * instruction, Instruction * instructions);
+Instruction * DeclarationInstructionSemanticAction(Declaration * declaration);
+Instruction * AssignationInstructionSemanticAction(Assignation * assignation);
+Instruction * ExpressionInstructionSemanticAction(Expression * expression);
+Instruction * PrintInstructionSemanticAction(Print * print);
+Instruction * FunctionCallInstructionSemanticAction(FunctionCall * functionCall);
+Instruction * ReturnStatementInstructionSemanticAction(ReturnStatement * returnStatement);
+Instruction * FunctionInstructionSemanticAction(Function * function);
+Instruction * ConditionalInstructionSemanticAction(Conditional * conditional);
+Instruction * LoopInstructionSemanticAction(Loop * loop);
+Declaration * DeclarationSemanticAction(Type * type, char * varName, Assignation * assignation);
+Type * IntTypeSemanticAction();
+Type * BoolTypeSemanticAction();
+Type * StringTypeSemanticAction();
+Assignation * AssignationSemanticAction(char * varName, Expression * expression);
+Expression * ArithmeticExpressionSemanticAction(ArithmeticExpression * arithmeticExpression);
+Expression * BooleanExpressionSemanticAction(BooleanExpression * booleanExpression);
+Expression * StringExpressionSemanticAction(StringExpression * stringExpression);
+ArithmeticExpression * AdditionExpressionSemanticAction(ArithmeticExpression * left, ArithmeticExpression * right);
+ArithmeticExpression * SubtractionExpressionSemanticAction(ArithmeticExpression * left, ArithmeticExpression * right);
+ArithmeticExpression * MultiplicationExpressionSemanticAction(ArithmeticExpression * left, ArithmeticExpression * right);
+ArithmeticExpression * DivisionExpressionSemanticAction(ArithmeticExpression * left, ArithmeticExpression * right);
+ArithmeticExpression * VarNameArithmeticExpressionSemanticAction(char * varName);
+ArithmeticExpression * IntegerArithmeticExpressionSemanticAction(int value);
+BooleanExpression * AndExpressionSemanticAction(BooleanExpression * left, BooleanExpression * right);
+BooleanExpression * OrExpressionSemanticAction(BooleanExpression * left, BooleanExpression * right);
+BooleanExpression * NotExpressionSemanticAction(BooleanExpression * expression);
+BooleanExpression * ComparisonExpressionSemanticAction(ArithmeticExpression * left, CompareOperator * op, ArithmeticExpression * right);
+BooleanExpression * VarNameBooleanExpressionSemanticAction(char * varName);
+BooleanExpression * BoolLiteralExpressionSemanticAction(bool value);
+StringExpression * StringLiteralExpressionSemanticAction(char * value);
+Print * PrintSemanticAction(Expression * expression);
+Function * FunctionSemanticAction(Type * returnType, char * functionName, Parameters * parameters, Block * block);
+Parameters * SingleParameterSemanticAction(Parameter * parameter);
+Parameters * MultipleParametersSemanticAction(Parameter * parameter, Parameters * parameters);
+Parameter * ParameterSemanticAction(Type * type, char * varName);
+FunctionCall * FunctionCallSemanticAction(char * functionName, Arguments * arguments);
+Arguments * SingleArgumentSemanticAction(Argument * argument);
+Arguments * MultipleArgumentsSemanticAction(Argument * argument, Arguments * arguments);
+Argument * ArgumentSemanticAction(Expression * expression);
+Conditional * ConditionalSemanticAction(BooleanExpression * condition, Block * ifBlock, Block * elseBlock);
+Loop * LoopSemanticAction(char * varName, ArithmeticExpression * start, ArithmeticExpression * end, Block * block);
+CompareOperator * GreaterThanSemanticAction();
+CompareOperator * LessThanSemanticAction();
+CompareOperator * EqualsEqualsSemanticAction();
+CompareOperator * NotEqualsSemanticAction();
+CompareOperator * GreaterEqualsSemanticAction();
+CompareOperator * LessEqualsSemanticAction();
+ReturnStatement * ReturnStatementSemanticAction(Expression * expression);
 
 #endif
